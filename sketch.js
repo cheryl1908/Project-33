3a13d842c894e4db31f49e97064ee9bbd887eced
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2021-04-05 15:47:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,85 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies; 
+var balls = [];
+var plinkos = [];
+var divisions =[];
+var ball;
 
-var particles=[]
-var plinkos=[]
-var divisions=[]
-var divisionHeight=[]
+var divisionHeight=300;
+var score =0;
 
 function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
 
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+    for (var j = 75; j <=width; j=j+50) {
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,175));
+    }
+
+    for (var j = 75; j <=width; j=j+50) {
+        plinkos.push(new Plinko(j,275));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,375));
+    }
+    
 }
-
-var ground1 = new Ground(700,height,800,20);
-var divison =new Divisons(30,200,20,400)
-
+ 
 function draw() {
-  createCanvas(800,400);
+  background("black");
+  textSize(35)
+  text("Score : "+score,20,40);
+  fill("white");
 
-  background(0);
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  text(" 200 ", 640, 550);
+  text(" 200 ", 720, 550);
+  Engine.update(engine);
+  ground.display();
 
-  for(var i=0;i<=width;i=i+80){
-    divisions.push(new Divisons(i,height-divisionHeight/2,10,divisionHeight));
+
+  for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();  
   }
+ 
+    if(ball!=null)
+    {
+       ball.display();
+  
+  
+    }
 
-
-  for(var i=40;i<=width;i=i+50){
-    plinkos.push(new Plinko(i,75));
-  }
-  for(var i=15;i<=width-10;i=i+50){
-    plinkos.push(new Plinko(i,175));
-  }
-
-  for(var i=0;i<particles.length;i++){
-    particles.display();
-  }
-
-
-  for(var i=0;i<divisions.length;i++){
-    divisons.display();
-  }
-  ground1.display();
-  divison.display();
+   for (var k = 0; k < divisions.length; k++) 
+   {
+     divisions[k].display();
+   }
+ 
 }
+
+
+function mousePressed()
+{
+  ball=new Ball(mouseX, 10, 10, 10); 
+  score++ 
+}
+
